fb0f240613cc546872bcef5d503656c0eb92072f
jonasitojonasito <jonasssito@hotmail.com>
2014-07-16 16:09:00 +0100

    Update TpyoCoinBankDedicated.sh

--- TpyoCoinBankDedicated.sh
+++ TpyoCoinBankDedicated.sh
@@ -10,12 +10,12 @@ while true; do
 		mkdir $file_conts;
 		mv $file_name $file_conts; 
 		file_name1=${file_name}.tar.gz;
-		file_name2=${file_name}.tpyo;
 		file_name3=${file_name}.tar.gz.gpg;
 		tar -zcvf $file_name1 $file_conts;
 		gpg --encrypt --recipient 'C513 B610 5490 19FE 83EB DCB2 322F 7280 070D 501E' $file_name1;
-		mv $file_name1 $file_name2;
-		rm $file_name3
+		rm $file_name1
+		chmod go-w $file_name3
 		sleep 1
              
+done
 done
